e17b3b157009a143ead08eec305b94fe0b08adc7
therealmarvin <nakedtoothbrush@hotmail.com>
2025-12-17 19:33:57 -0500

    update conv embeddings

--- models/vision_transformer/conv_embedding.py
+++ models/vision_transformer/conv_embedding.py
@@ -8,11 +8,11 @@ class ConvEmbedding(nn.Module):
         self.patch_size = patch_size
         self.embedding_size = embedding_size
 
-        self.conv_1 = nn.Conv2d(nb_channels, embedding_size, kernel_size=patch_size[0], stride=patch_size[0])
+        self.conv_1 = nn.Conv2d(nb_channels, embedding_size, kernel_size=patch_size, stride=patch_size)
 
     def forward(self, x):
         x = self.conv_1(x)
         x = x.view(x.shape[0], x.shape[1], -1)
-        # need to transpose because x.shape[1] is the embedding size (out channels) and x.shape[2] is the patch count
+
         x = x.transpose(1, 2)
         return x

de3b56215f640a76cb712503c0a57f0d1be1de3b
therealmarvin <nakedtoothbrush@hotmail.com>
2025-12-22 10:33:01 -0500

    bit of a cleanup

--- main_ijepa.py
+++ main_ijepa.py
@@ -7,17 +7,18 @@ from models.vision_transformer.vision_transformer_classifier import ViTClassifie
 
 
 def main_ijepa():
+    train_set, test_set, image_size = get_mnist_sets()
+
     summary = SummaryWriter()
     patch_size = (4, 4)
     embedding_size = 64
 
-    embedding_layer = ConvEmbedding(1, patch_size=patch_size, embedding_size=embedding_size)
-    model = ViTClassifier(embedding_layer=embedding_layer, img_size=(1, 28, 28),
+    embedding_layer = ConvEmbedding(image_size[0], patch_size=patch_size, embedding_size=embedding_size)
+    model = ViTClassifier(embedding_layer=embedding_layer, img_size=image_size,
                           nb_output=10, nb_encoder_blocks=6, nb_heads=4)
 
     print(model)
     train_config_file = "config/training_params.ini"
-    train_set, test_set, image_size = get_mnist_sets()
     run_specific_experiment(summary, model, (train_set, test_set), train_config_file)
     summary.close()
 

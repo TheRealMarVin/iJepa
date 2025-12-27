8650e10d2c5ff6a2f8e581054d198c0f6ca8492a
therealmarvin <nakedtoothbrush@hotmail.com>
2025-12-27 07:41:55 -0500

    we need a predictor

---
+++ models/vision_transformer/predictor.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+
+from models.vision_transformer.transformer_encoder_block import TransformerEncoderBlock
+
+
+class Predictor(nn.Module):
+    def __init__(self, embedding_dim, nb_layers, nb_heads, mlp_ratio=4.0):
+        super().__init__()
+
+        self.blocks = nn.ModuleList([
+            TransformerEncoderBlock(embedding_dim=embedding_dim, num_heads=nb_heads, mlp_ratio=mlp_ratio)
+            for _ in range(nb_layers)
+        ])
+
+        self.norm = nn.LayerNorm(embedding_dim)
+
+    def forward(self, x):
+        for block in self.blocks:
+            x = block(x)
+        return self.norm(x)

5ff35d438fd882ffdaeae4c81c078b14cc53cac9
therealmarvin <nakedtoothbrush@hotmail.com>
2026-01-01 11:51:07 -0500

    classifier using jepa

---
+++ models/vision_transformer/ijepa_classifier.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+from models.vision_transformer.classifier import Classifier
+from models.vision_transformer.vision_transformer import VisionTransformer
+
+
+class IJEPAClassifier(nn.Module):
+    def __init__(self, backbone, embedding_dim, nb_output):
+        super(IJEPAClassifier, self).__init__()
+        self.backbone = backbone
+        self.classifier = nn.Linear(embedding_dim, nb_output)
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = x.mean(dim=1)
+        return self.classifier(x)

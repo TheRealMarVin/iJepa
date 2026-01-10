8c124293d7d38d82c45de99a38472bac8c967098
TheRealMarvin <nakedtoothbrush@hotmail.com>
2026-01-09 19:38:06 -0500

    make it work with STL10

--- main_conv_vit.py
+++ main_conv_vit.py
@@ -1,24 +1,25 @@
 from torch.utils.tensorboard import SummaryWriter
 
 from common_training_setup import run_specific_experiment
-from helpers.dataset_helpers import get_mnist_sets
+from helpers.dataset_helpers import get_mnist_sets, get_stl10_sets
 from models.vision_transformer.conv_embedding import ConvEmbedding
 from models.vision_transformer.vision_transformer_classifier import ViTClassifier
 
 
 def main_vit():
     summary = SummaryWriter()
-    patch_size = (4, 4)
+    patch_size = (16, 16)
     embedding_size = 64
     nb_output = 10
+    train_set, test_set, image_size = get_stl10_sets()
 
-    embedding_layer = ConvEmbedding(1, patch_size=patch_size, embedding_size=embedding_size)
-    model = ViTClassifier(embedding_layer=embedding_layer, img_size=(1, 28, 28),
+    embedding_layer = ConvEmbedding(image_size[0], patch_size=patch_size, embedding_size=embedding_size)
+    model = ViTClassifier(embedding_layer=embedding_layer, img_size=image_size,
                           nb_output=nb_output, nb_encoder_blocks=6, nb_heads=4)
 
     print(model)
     train_config_file = "config/training_params.ini"
-    train_set, test_set, image_size = get_mnist_sets()
+
     run_specific_experiment(summary, model, (train_set, test_set), train_config_file)
     summary.close()
 
